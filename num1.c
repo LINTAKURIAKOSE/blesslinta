18f6fc8ff6321536f69257158b3e1e21070fe1a2
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-08-25 19:17:17 +0530

    Create num1.c

---
+++ num1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter thenumber");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf("number is even");
+}
+else
+{
+printf("number is odd");
+}
+}
