982b95fab856bb8d0a11ed972ff98cf764956557
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-08-31 19:40:16 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char n;
+printf("enter the characters");
+scanf(%d",&n);
+if(n<='a'&&n<='z'||n<='A'&&n<='Z')
+{
+printf("the character is alphabet");
+}
+else
+{
+printf("the character is not alphabet");
+}
+}
