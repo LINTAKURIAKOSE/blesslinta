04da0c87c1945f435e36bac7b264235e575586b8
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-01 19:21:14 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i,sum==o;
+printf("enter positive number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("sum=%d",sum);
+return 0;
+}
