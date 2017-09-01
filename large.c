a4cdbd4e720044ad8db8b6e8492e133f848779a3
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-01 18:50:32 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the three numbers");
+scanf("%d%d%d",&a,&b,&c);
+if(a>=b&&a>=c)
+{
+printf("a is largest number");
+}
+elseif(b>=a&&b>=c)
+{
+printf("b is largest number");
+}
+else
+{
+printf("c is largest number");
+}
+}
