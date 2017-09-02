7080dc281832a9803e677b8daac1ea53b68882e8
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-02 19:26:20 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float base,exp,sum;
+printf("enter the base number");
+scanf(%f",&base);
+printf("enter the exponent");
+scanf("%f",&exp);
+sum=pow(base,exp);
+printf("%f the sum is",sum);
+return 0;
+}
+
