99af6a402f34b12f8acaa94397bd08783633473d
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-03 16:03:52 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i;
+printf(" enter  numbers");
+scanf("%d",&n);
+for(i=1;1<n;i++)
+{
+if(i%2!=0)
+{
+printf("display odd numbers");
+}
+}
+return 0;
+}
