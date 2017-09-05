2ea81b6581e9eaf52d82fb57458d2c3a136eb7af
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-05 19:42:50 +0530

    Rename intervel.c to interve1.c

---
+++ interve1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>  
+int main() {  
+      int count; 
+     printf(" enter even numbers between 1 to 100");  
+    scanf("%d",&count);
+    for(count= 1; count<= 100; count++) {  
+    if(counter%2 ==0)
+     printf("%d ", counter);   }  
+  }  
+    return 0;  
+} 
