16dd5473a987e007aeae2c8c6851e4296cdbdd25
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-08 18:50:46 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char arr[100];
+printf("enter a strig to reverse");
+gets(arr);
+strrev(arr);
+printf("reversed string is");
+return 0;
+}
