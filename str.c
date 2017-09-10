f588bd141b73c993ed641fddcf8a643db6bf54a2
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-10 15:54:52 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+ int main()
+ {
+  char inputString[20];
+  printf("Enter a String for Integer conversion \n");
+  gets(inputString);
+ printf("Integer: %d \n", atoi(inputString));
+    return 0;
+}
