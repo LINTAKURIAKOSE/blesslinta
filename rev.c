74b137b8686b1d21e5acc541173fd26509adbff6
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-12 16:08:43 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n,reverse=0,remainder;
+	printf("enter the number");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+	    remainder=n%10;
+	    remainder=reverse*10+remainder;
+	    n=n/10;
+	}
+	return 0;
+}
