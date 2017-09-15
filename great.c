74760da2b2a0738bb929926fc664d93075de3b2c
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-15 19:22:57 +0530

    Create great.c

---
+++ great.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+void main()
+{
+    int n1, n2, n3;
+    printf("Enter the values of num1, num2 and num3\n");
+    scanf("%d%d%d",&n1,&n2,&n3);
+    printf("n1 = %d\tn2 = %d\tn3 = %d\n", n1, n2, n3);
+    if (n1 > n2)
+    {
+        if (n1 > n3)
+        {
+            printf("n1 is the greatest among three \n");
+        }
+        else
+        {
+            printf("n3 is the greatest among three \n");
+        }
+    }
+    else if (n2 > n3)
+        printf("n2 is the greatest among three \n");
+    else
+        printf("n3 is the greatest among three \n");
+}
