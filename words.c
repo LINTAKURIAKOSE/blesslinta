6b13dffad39ee1000ea3f2b7befb06e6fbdbd3ee
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-09-24 14:55:37 +0530

    Create words.c

---
+++ words.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+ 
+void main()
+{
+    char n[200];
+    int count = 0, i;
+ 
+    printf("enter the string\n");
+    scanf("%s", n);
+    for (i = 0;s[i] != '\0';i++)
+    {
+        if (s[i] == ' ')
+           count++;    
+    }
+    printf("number of words in a string are: %d\n", count +1);
+}
