8bd768af63e720b19154f3ef8db970c505d91355
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2017-10-03 19:15:02 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str;
+printf("the numeric characters in a string is");
+scanf("%s",&s);
+str = "this2009";  
+printf(" str.isnumeric()");
+str = "23443434";
+printf( "str.isnumeric()");
+printf("count number of characters");
+return 0;
+}
