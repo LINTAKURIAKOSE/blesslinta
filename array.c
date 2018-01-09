616f3bbcf07e226b290bc0d0023d9135acb64e7b
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-01-09 16:52:58 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a[30],i,j,n;
+printf("enter the array");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("array is")
+}
+for(j=0;j<=n;j++)
+{
+printf("%d",a[i]);
+}
+}
