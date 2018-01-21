9b33f6dc04935d6b49281fd8c3036ae522979400
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-01-21 12:17:53 +0530

    Rename bit.c to bit1.c

---
+++ bit1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int a,int b)
+{
+    a=a^b;
+    b=a^b;
+    a=b;
+    }
+void main()
+{
+    int a,b;
+    printf("enter numbers");
+    scanf("%d%d",&a,&b);
+    printf("swap numbers\t%d\t%d",b ,a);
+    
+}
