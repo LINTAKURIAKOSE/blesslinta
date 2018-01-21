19b7c4a41cc4ae10794615d874bd956677cce96a
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-01-21 11:54:56 +0530

    Create line1.c

---
+++ line1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+
+
+{ 
+    char para[30],i,line=0,n; 
+     printf("Enter your paragraph\n\n\t: ");
+     scanf("%d",&n);
+    for(i=0;i<=1;i++)
+    if(para[i]='.')
+    {
+        line++;
+    }
+    printf("number of sentence%d",line);
+}
