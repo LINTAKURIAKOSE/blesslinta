b0bf252f12c3048823783e169613a84d2c94cdb6
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-01-23 16:54:15 +0530

    Create l.c

---
+++ l.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+  int main()
+  {
+    int a[10];
+    int i;
+    int g;
+    printf("Enter ten values:");
+	scanf("%d",&a[i]);
+    for (i = 0; i < 10; i++)
+     g = a[0];
+    for (i = 0; i < 10; i++) 
+        (a[i] > g)
+	{
+     printf(" lagest of ten numbers is %d",g);
+    return 0;
+  }
+}
