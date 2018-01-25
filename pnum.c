82158f0480c0067477e39efc830957592586b99e
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-01-25 16:29:33 +0530

    Create pnum.c

---
+++ pnum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int n,count=0,i;
+printf("enter the number");
+scanf("%d",&n);
+for(i=2;i<n;i++)
+{
+if(n%i==0)
+{
+count++;
+break;
+}
+}
+if(count==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
