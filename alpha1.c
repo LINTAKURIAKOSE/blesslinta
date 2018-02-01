773f21f56bbb9338942fdd8386ad4786596f38c6
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-02-01 08:45:18 +0000

    Create alpha1.c

---
+++ alpha1.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+void main()
+{
+    char a[10],s;
+    int i,count1=0,count2=0;
+    scanf("%[^\n]c",&a);
+    for(i=0;i!="\0";i++)
+    {
+
+    if(s[i]>'a'&&s[i]<'z'||s[i]>'A'&&s[i]<'Z')
+    {
+        count2++;
+    }
+if((count1>0)(count2>0))
+    {
+        printf("yes\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+    
+}
+
+}
