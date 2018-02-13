16e226c20d7d5e5ed6a355830a39651fbc8721b4
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-02-13 16:23:35 +0530

    Create num_sum.c

---
+++ num_sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,i,sum=0,a[10];
+printf("enter the number\n");
+scanf("%d",&n);
+printf("number ofintegers\n");
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+scanf("%d",&a[i]);
+printf(" %d",sum);
+}
+
