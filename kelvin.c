a0f68e9e1b13b5baae7c80a238ac39afdf5415fd
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-02-22 16:31:36 +0530

    Create kelvin.c

---
+++ kelvin.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int c,k;
+  printf("enter celsius\n");
+  scanf("%d",&c);
+ if(c!=0)
+  {
+    k=c+273;
+    printf("%d",k);
+  }
+  else
+  {
+  return 0;
+  }
+}
