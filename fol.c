c0e6d019b43a70b7d38e5897bede36e70c088c78
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-02-22 15:51:14 +0530

    Create fol.c

---
+++ fol.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int n,k,l;
+  printf("enter n number\n");
+  scanf("%d%d",&n,&k);
+  printf("numbers\n");
+  scanf("%d",&l);
+  if(k<4)
+  {
+    printf("%d",k);
+  }
+  else
+  {
+    return 0;
+  }
+}
