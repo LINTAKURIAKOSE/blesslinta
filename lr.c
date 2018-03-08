2aae996bff71de9d43b3442cff77ab992d4b6475
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-03-08 15:43:20 +0530

    Create lr.c

---
+++ lr.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int l,r,n;
+  printf("enter n \n");
+  scanf("%d",&n);
+  printf("enter l&r\n");
+  scanf("%d%d",&l,&r);
+ if(l<n&&r>n)
+ {
+   printf("yes");
+ }
+ else
+ {
+   printf("no");
+ }
+ return 0;
+
+}
