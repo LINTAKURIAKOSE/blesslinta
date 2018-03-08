3d75aa12724cb42fe2f5fc953d1eff2431c94971
LINTAKURIAKOSE <30828454+LINTAKURIAKOSE@users.noreply.github.com>
2018-03-08 16:32:02 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a=65,n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+      a++;
+    printf("\n");
+    }
+     
+        printf("%c",a-1);
+    }
+
+ 
